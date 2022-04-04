c6103691258910b75873fc8154148620fac1d431
delecrth <thomas.delecroix@reseau.eseo.fr>
2022-04-04 18:46:52 +0200

    ajout de commentaires

--- app/src/main/java/com/example/eseo_minesweeper/PlayerRanking.java
+++ app/src/main/java/com/example/eseo_minesweeper/PlayerRanking.java
@@ -1,17 +1,21 @@
 package com.example.eseo_minesweeper;
 
+//Création de la classe représentant une ligne du classement
 public class PlayerRanking implements java.io.Serializable{
 
+    //Déclaration des variables
     private String rank;
     private String name;
     private String time;
 
+    //Constructeurs de la classe
     public PlayerRanking(String _rank, String _name, String _time){
         this.rank = _rank;
         this.name = _name;
         this.time = _time;
     }
 
+    //Les Setters et Getters pour chaque variable
     public void setName(String name) {
         this.name = name;
     }
