bb0655cecc6eea8ba8a7d57fe85afe8daa82ae0d
chupinal <alexischup@gmail.com>
2022-04-10 19:24:25 +0200

    Rank is ok

--- app/src/main/java/com/example/eseo_minesweeper/CustomAdapter.java
+++ app/src/main/java/com/example/eseo_minesweeper/CustomAdapter.java
@@ -43,24 +43,24 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
     //Création d'une classe MyViewHolder représentant une ligne du classement
     public class MyViewHolder extends RecyclerView.ViewHolder {
         //Déclaration des TextView
-        private TextView mRank;
-        private TextView mPlayerName;
-        private TextView mTime;
+        private TextView mScore;
+        private TextView mSeconds;
+        private TextView mMinutes;
 
         //Constructeur de la classe
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            mRank = itemView.findViewById(R.id.textViewScore);
-            mPlayerName = itemView.findViewById(R.id.textViewSeconds);
-            mTime = itemView.findViewById(R.id.textViewMinutes);
+            mScore = itemView.findViewById(R.id.textViewScore);
+            mSeconds = itemView.findViewById(R.id.textViewSeconds);
+            mMinutes = itemView.findViewById(R.id.textViewMinutes);
         }
 
         //On affiche les infos correspondantes à chaque TextView
         public void display(PlayerRanking playerRanking){
-            mRank.setText(playerRanking.getScore());
-            mPlayerName.setText(playerRanking.getSeconds());
-            mTime.setText(playerRanking.getMinutes());
+            mScore.setText(playerRanking.getScore());
+            mSeconds.setText(playerRanking.getSeconds());
+            mMinutes.setText(playerRanking.getMinutes());
         }
     }
 }
